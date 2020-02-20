0baf91a55cc14d9e80d5731bb6286ee3b00053c0
chewy <haianben@outlook.com>
2020-02-20 15:57:05 +0800

    TTest

---
+++ TTest/TTest/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TTest
+//
+//  Created by 贲诚伟 on 2020/2/20.
+//  Copyright © 2020 贲诚伟. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
